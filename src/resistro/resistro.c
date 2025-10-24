96c31310d5e9f7614e4f088fa86e13960abe8ae0
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-23 23:27:28 -0300

    Create resistro.c

---
+++ src/resistro/resistro.c
@@ -0,0 +1,13 @@
+// src/resistro/resistro.c
+// Definicoes e inicializacoes das variaveis de registro do sistema.
+
+#include "resistro.h"
+
+// Definicoes das variaveis declaradas em resistro.h
+// Usamos 'const char* const' para garantir que nao sejam modificadas em tempo de execucao.
+
+const char* const OS_NAME_VERSION = "OS:ArcanOS";
+
+const char* const DEFAULT_USER = "user_arcan";
+
+const char* const VENDOR_NAME = "unknown inc.";
