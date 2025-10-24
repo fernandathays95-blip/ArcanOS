1de7c974a96360ceb155c201fe493bc1dc0c317d
fernandathays95-blip <fernandathays95@gmail.com>
2025-10-23 21:36:39 -0300

    Create clear.sh

---
+++ src/clear/clear.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script: clear.sh
+# Finalidade: Simplesmente limpa a tela do terminal.
+# Pode ser usado para limpar o ambiente de build ou scripts.
+
+# O comando 'clear' envia a sequência de escape para o terminal
+# para apagar o conteúdo da tela.
+clear
